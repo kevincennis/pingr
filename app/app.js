07dba6aff36df796261c9642cf7b3c7b83a6a17b
Kevin Ennis <kennis84@gmail.com>
2012-03-09 21:33:08 -0500

    Indenting stuff

--- app/app.js
+++ app/app.js
@@ -1,21 +1,21 @@
-/* Config */
+// Config
 var http = require('http'),
-	url = require('url'),
-	path = require('path'),
-	static = require('node-static'),
-	webroot = path.join(path.dirname(__filename), '../public'),
-	index = '/index.html',
-	file = new(static.Server)(webroot);
-/* Server */
+    url = require('url'),
+    path = require('path'),
+    static = require('node-static'),
+    webroot = path.join(path.dirname(__filename), '../public'),
+    index = '/index.html',
+    file = new(static.Server)(webroot);
+// Server
 http.createServer(function(req, res){
-	var uri = url.parse(req.url).pathname,
-		filename = path.join(webroot, uri == '/' ? index : uri);
+    var uri = url.parse(req.url).pathname,
+        filename = path.join(webroot, uri == '/' ? index : uri);
 	path.exists(filename, function(exists){
-    	if (!exists){
-    		res.writeHead(200, {'Content-Type': 'text/plain'});
-	    	res.end();
-    	} else {
-    		file.serve(req, res);
-    	}
-    });
+        if (!exists){
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end();
+        } else {
+            file.serve(req, res);
+        }   
+    }); 
 }).listen(80);
